b451f0676b78c246a20685b3afb5d8a6506aad6b
Stephen Townsend <abordercollie@gmail.com>
2018-04-08 23:05:32 -0300

    1. App.js - Second fix for the loading of the connecting IP and Port to TSX. 2. Loading Filter names and slots from TSX 3. Tied filters into their series 4. Working on the focsFilter.

--- imports/api/filters.js
+++ imports/api/filters.js
@@ -2,4 +2,21 @@ import { Mongo } from 'meteor/mongo';
 
 // Used to store the filters currently available/active on TSX
 export const Filters = new Mongo.Collection('filters');
-export const FilterWheels = new Mongo.Collection('filterWheels');
+
+
+// *******************************
+// Get the filters from TheSkyX
+Filters.helpers({
+
+  renderDropDownFilters: function() {
+    var filters = Filters.find().fetch();
+    var filterArray = [];
+    for (var i = 0; i < filters.length; i++) {
+      filterArray.push({
+        key: filters[i]._id,
+        text: filters[i].name,
+        value: filters[i].name });
+    }
+    return filterArray;
+  },
+});
