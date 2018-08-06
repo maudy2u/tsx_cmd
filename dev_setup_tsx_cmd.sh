38bf5ffbb092e50f4997f9cf30bda2e81f82e4c3
Stephen Townsend <abordercollie@gmail.com>
2018-08-06 19:05:30 -0300

    1. added fix for less and semantic-ui

--- dev_setup_tsx_cmd.sh
+++ dev_setup_tsx_cmd.sh
@@ -20,23 +20,17 @@
 # alias meteor=$HOME/meteor/meteor
 
 # PREPARE THE NEW METEOR PROJECT
-~/meteor/meteor npm install --save-dev babel-plugin-transform-class-properties@6.24.1
 ~/meteor/meteor add semantic:ui@=2.2.6_5 flemay:less-autoprefixer jquery
-~/meteor/meteor npm install --save shelljs
-~/meteor/meteor add flemay:less-autoprefixer jquery
-~/meteor/meteor npm install --save react@15.6.2 react-dom@15.6.2 semantic-ui-react@0.78.3
-
 ~/meteor/meteor add react-meteor-data
-~/meteor/meteor add froatsnook:sleep package-stats-opt-out
-~/meteor/meteor npm install --save @babel/runtime
-~/meteor/meteor npm install --save react-simple-range react-datetime-bootstrap react-timekeeper
-~/meteor/meteor add ostrio:logger ostrio:loggerconsole ostrio:loggerfile
-~/meteor/meteor add ostrio:meteor-root
-~/meteor/meteor add ostrio:loggermongo
+~/meteor/meteor add froatsnook:sleep package-stats-opt-out akasha:shelljs session
+~/meteor/meteor add ostrio:logger ostrio:loggerconsole ostrio:loggerfile ostrio:meteor-root ostrio:loggermongo
 ~/meteor/meteor add dburles:collection-helpers
 ~/meteor/meteor add vsivsi:job-collection
-~/meteor/meteor add session
-~/meteor/meteor add akasha:shelljs
+~/meteor/meteor npm install --save @babel/runtime react-simple-range react-datetime-bootstrap react-timekeeper react@15.6.2 react-dom@15.6.2 shelljs
+~/meteor/meteor npm install --save-dev babel-plugin-transform-class-properties@6.24.1
+~/meteor/meteor npm install --save semantic-ui-react@0.78.3
+# used for a semantic-ui less error
+~/meteor/meteor npm install --save-dev postcss@6.0.22 postcss-load-config@1.2.0
 
 # meteortyos breaks job-collection
 # ~/meteor/meteor add meteortoys:allthings
