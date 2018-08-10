160dd5f723d448ed117a12b9cd0485838480793d
Stephen Townsend <abordercollie@gmail.com>
2018-08-09 22:57:06 -0300

    1. fix for the aarch64 and armhf install - added the dev environments.

--- tsx_cmd_install.sh
+++ tsx_cmd_install.sh
@@ -92,7 +92,8 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
       echo "deb [ arch=amd64,arm64,ppc64el,s390x ] http://repo.mongodb.com/apt/ubuntu xenial/mongodb-enterprise/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list
       sudo apt update
-      sudo apt install mongodb-enterprise
+      sudo apt install g++ build-essential -y
+      sudo apt install mongodb-enterprise -y
 
       echo " *******************************"
       echo "nodejs - Download and extract"
@@ -111,7 +112,8 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       echo "Mongodb - apt-get install"
       echo " *******************************"
       sudo apt update
-      sudo apt install mongodb-server
+      sudo apt install g++ build-essential -y
+      sudo apt install mongodb-server -y
 
       echo " *******************************"
       echo "nodejs - Download and extract"
