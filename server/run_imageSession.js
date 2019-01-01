762cbb47bfb291fe614ec2203c31c5054f5b5887
Stephen Townsend <abordercollie@gmail.com>
2019-01-01 12:19:02 -0400

    1. More fixes for the GitHub release

--- server/run_imageSession.js
+++ server/run_imageSession.js
@@ -869,8 +869,7 @@ function InitialFocus( target ) {
 
   var temp = tsx_RunFocus3( target ); // need to get the focus position
   tsxInfo( ' *** ' + target.targetFindName +': Initial Focus temp: ' + temp );
-  // var temp = result.split('|')[0].trim();
-  //  var temp = tsx_GetFocusTemp( target ); // temp and position set inside
+
   if( temp != '') {
     tsx_SetServerState( 'initialFocusTemperature', temp);
   }
